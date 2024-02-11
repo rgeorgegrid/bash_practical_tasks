112d92a6ad597ce6b494873442c9d26a778f08ea
Reuben George <rgeorge@griddynamics.com>
2024-02-11 15:13:53 +0530

    Q2. debug_flag

--- debug_flag.sh
+++ debug_flag.sh
@@ -41,13 +41,16 @@ case $OP in
     *)
         RES=500
         ;;
-esac
+esac 
 
-echo ${NUMS[@]}
-echo 
+RES=${NUMS[0]}
+flag=0
 
 for NUM in "${NUMS[@]}"; do
-    echo $NUM
+    if [ $flag -eq "0" ]; then
+        flag=1
+        continue
+    fi
     case $OP in
         "+")
             let "RES+=NUM"
